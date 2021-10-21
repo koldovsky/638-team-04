846edf63d45e5f4e8bd541808acc2da05231ad8c
Inna <91555726+InnaPol0704@users.noreply.github.com>
2021-10-22 00:12:30 +0300

    add js and start to make link for bottonns

---
+++ js/rent-offer.js
@@ -0,0 +1,70 @@
+(function() {
+
+    const products = [
+        {
+            id: 1,
+            titel: 'cabin-rentals',
+            decription: '<p>✅ 1 Bedroom</p>',
+            '<p>✅ 1 Bathroom</p>',
+            '<p>✅ Sleeps 2</p>',
+            '<p>Jetted Tub</p>'
+            '<p>Flat Panel TV</p>'
+            '<p>Fireplace</p>'
+            '<p>Wi-Fi</p>',
+            price: $65,
+            image: 'img/rent-offer/rever-chalet.webp'
+        },
+        {
+            id: 2,
+            titel: 'lookout-lodge',
+            decription: '<p>✅ 2 Bedrooms</p>',
+            '<p>✅ 1 Bathroom</p>',
+            '<p>✅ Sleeps 4</p>',
+            '<p>Hot Tub</p>'
+            '<p>Pool Tablet</p>'
+            '<p>Jacuzzi Tub</p>'
+            '<p>Wi-Fi</p>',
+            price: $75,
+            image: 'img/rent-offer/smoky-view-chalet.webp'
+        },
+        {
+            id: 3,
+            titel: 'lookout-lodge',
+            decription: '<p>✅ 2 Bedrooms</p>',
+            '<p>✅ 1 Bathroom</p>',
+            '<p>✅ Sleeps 4</p>',
+            '<p>Hot Tub</p>'
+            '<p>Pool Tablet</p>'
+            '<p>Jacuzzi Tub</p>'
+            '<p>Wi-Fi</p>',
+            price: $75,
+            image: 'img/rent-offer/lookout-lodge.webp'
+        },
+        {
+            id: 4,
+            titel: 'smoky-view-chalet',
+            decription: '<p>✅ 2 Bedrooms</p>',
+            '<p>✅ 2 Bathrooms</p>',
+            '<p>✅ Sleeps 6</p>',
+            '<p>Whirpool Tub</p>'
+            '<p>Air Hockey</p>'
+            '<p>Fireplace</p>'
+            '<p>Wi-Fi</p>'
+            price: $85,
+            image: 'img/rent-offer/smoky-view-chalet.webp'
+        },
+        {
+            id: 5,
+            titel: 'Mountain Haven',
+            decription: '<p>✅ 3 Bedrooms</p>',
+            '<p>✅ 2 Bathrooms</p>',
+            '<p>✅ Sleeps 8</p>',
+            '<p>Whirpool Tub</p>'
+            '<p>Grill</p>'
+            '<p>Fireplace</p>'
+            '<p>Wi-Fi</p>',
+            price: $100,
+            image: 'img/rent-offer/mountain-splash.webp'
+        },
+
+})(); 
