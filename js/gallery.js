ed72c694c90bbe4482da4de07d3b0be9fdf3fcea
MykolaRybaruk <m.rybaruk@gmail.com>
2021-10-27 20:22:44 +0000

    added js code

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,44 @@
+(function() {
+    const navigation = [
+        {
+            id: 1,
+            row_nav: SUMMER,
+        },
 
+        {
+            id: 1,
+            row_nav: WINTER,
+        },
+
+        {
+            id: 1,
+            row_nav: SPRING,
+        },
+
+        {
+            id: 1,
+            row_nav: AUTUMN,
+        },
+
+        {
+            id: 1,
+            row_nav: FISHING,
+        },
+
+        {
+            id: 1,
+            row_nav: SPA,
+        }
+    ];
+
+    function renderMenu(navigation) {
+        const navContainer = document.querySelector('.gallery_nav');
+        for (const item of navigation) {
+            gallery_nav.innerHTML += `
+            <a class="row_nav" href="#">${navigation.row_nav}</a>
+            `;
+        }
+    }
+
+    renderMenu(navigation);
+})();
