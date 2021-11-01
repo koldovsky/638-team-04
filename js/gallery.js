3b5e9fb60913b6a8b46ae6b5ff3ec1a46df1fc77
MykolaRybaruk <m.rybaruk@gmail.com>
2021-11-01 20:03:49 +0000

    fixing js

--- js/gallery.js
+++ js/gallery.js
@@ -1,43 +1,42 @@
 (function () {
     const navItems = [
-        {
-            id: 1,
-            rowNaw: 'summer'
-        },
+    {
+        id: 1,
+        rowNaw: 'summer'
+    },
 
-        {
-            id: 2,
-            rowNaw: 'winter'
-        },
+    {
+        id: 2,
+        rowNaw: "winter"
+    },
 
-        {
-            id: 3,
-            rowNaw: 'spring'
-        },
+    {
+        id: 3,
+        rowNaw: "spring"
+    },
 
-        {
-            id: 4,
-            rowNaw: 'autumn'
-        },
+    {
+        id: 4,
+        rowNaw: "autumn"
+    },
 
-        {
-            id: 5,
-            rowNaw: 'fishing'
-        },
+    {
+        id: 5,
+        rowNaw: "fishing"
+    },
 
-        {
-            id: 6,
-            rowNaw: 'spa'
-        },
+    {
+        id: 6,
+        rowNaw: "spa"
+    },
+
     ];
 
-    function renderMenu(navItems) {
-        const menuContainer = document.querySelector('.gallery_nav');
+    function renderMenu() {
+        const gallery_nav = document.querySelector('.gallery_nav');
         for (const item of navItems) {
-            gallery_nav.inner.HTML += `
-            <a class="row_nav" href="#">${navItems.rowNaw}</a>
-            `
-        }
+            gallery_nav.innerHTML += `<a class="row_nav" href="#">${navItems.rowNaw}</a>`
+        }   
     }
 
     renderMenu(navItems);
