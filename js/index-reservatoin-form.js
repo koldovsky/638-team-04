5153352a3fa331d01aa6405f56b3ebdc6810056e
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2021-11-02 16:37:25 +0200

    100% done

--- js/index-reservatoin-form.js
+++ js/index-reservatoin-form.js
@@ -1,30 +1,61 @@
 (function () {
-    const form = document.querySelector('.contact-form-nodejs');
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      const status = document.querySelector('.contact-form-status');
-    //   const data = new FormData(event.target);
-      const data = JSON.stringify({
-          name: document.querySelector('.contact-form-nodejs input[name=user-name]').value,
-          tel: document.querySelector('.contact-form-nodejs input[name=user-tel]').value,
-          datein: document.querySelector('.contact-form-nodejs input[name=user-datein]').value,
-          dateout: document.querySelector('.contact-form-nodejs input[name=user-dateout]').value
-      });
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
+  const things = [
+      {
+          id: 1,
+          image: 'img/index/things-to-do/horseriding.jpg',
+          title: 'Horse Riding',
+          price: '$4',
+          description: 'Take a measured romantic horseback riding tour and enjoy our unforgettable picturesque surroundings.'
+      },
+      {
+          id: 2,
+          image: 'img/index/things-to-do/rockclimbing.jpg',
+          title: 'Rock Climbing',
+          price: '$2',
+          description: 'Whether you are a professional mountaineer or it’s your first time, Yosemite one of the best places for this kind of sport.'
+      },
+      {
+          id: 3,
+          image: 'img/index/things-to-do/biking.jpg',
+          title: 'Biking',
+          price: '$1',
+          description: 'Rent a bike and take a fun and easy two-wheel tour over Yosemite Valley. It usually takes 2 hours.'
+      },
+      {
+          id: 4,
+          image: 'img/index/things-to-do/rafting.jpg',
+          title: 'Rafting',
+          price: '$3',
+          description: 'You have a perfect choice to combine your river rafting adventure with a visit to the park. Test our waters.'
+      },
+      {
+          id: 5,
+          image: 'img/index/things-to-do/fishing.jpg',
+          title: 'Fishing',
+          price: '$2',
+          description: 'With a number of permanent streams and mountain lakes, we offer anglers of all levels a diverse variety of opportunities for fishing.'
+      },
+      {
+          id: 6,
+          image: 'img/index/things-to-do/spas.jpg',
+          title: 'Spas and Wellness',
+          price: '$2',
+          description: 'After a long exhausting day, you can relax and pamper yourself in our unique spa and wellness center.'
+      }
+  ];
 
+  function renderBlock(things) {
+      const thingsContainer = document.querySelector('.things-block');
+      for (const thing of things) {
+          thingsContainer.innerHTML += `
+          <div class="things-card-blocks">
+              <img src="${thing.image}" alt="${thing.title}">
+              <h2>${thing.title}</h2>
+              <h3>${thing.price}/hour</h3>
+              <p>${thing.description}</p>
+          </div>
+          `
+      }
+  }
+  renderBlock(things);
 })();
